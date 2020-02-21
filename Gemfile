29309776880916797af93989091a9810e7c7bc70
kealanheena <kealanheena@gmail.com>
2020-02-21 14:46:56 +0000

    Chore: make films appear in order of release date (new - old)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'httparty'
-# gem 'rubygems'
 
 group :development, :test do
   gem "capybara"
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,7 +1,15 @@
 feature "Index" do
+  let(:first_film) { "When Marnie Was There" }
+  let(:second_film) { "The Tale of the Princess Kaguya" }
+
   scenario "Index page displays the titles of Ghibli movies" do
     visit("/")
     expect(page).to have_content("My Neighbor Totoro")
     expect(page).to have_content("Only Yesterday")
+  end
+
+  scenario "Index page displays the titles in order of release date(new - old)" do
+    visit("/")
+    expect(first_film).to appear_before(second_film, only_text: true)
   end
 end
